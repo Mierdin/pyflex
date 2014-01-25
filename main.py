5c56fabb77bf28e7591ed80c764b4b06c078273a
Matt Oswalt <matt@keepingitclassless.net>
2014-01-24 22:27:26 -0500

    Created most UCS functions at a basic level. Just used ConvertToPython() to populate each function - will definitely need to go back through and remove bloat, enhance, etc.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from UcsSdk import *
-handle = UcsHandle()
-handle.Login("10.102.1.5", "admin", "password")
+##VERY MUCH A WORK IN PROGRESS. This is the main script that will call the modules More soon.
 
-thisOrg = "org-root/org-DI_DCA"
+from UcsSdk import *
+
 
 orgObj = handle.GetManagedObject(None, OrgOrg.ClassId(), {OrgOrg.DN : thisOrg})
+
+
+
+
